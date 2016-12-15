afcaafbb4c7c1f89f21acbf4c89cc5251e995230
Nagen-Mac <nagen@mac.com>
2016-12-15 01:05:10 -0500

    Idea Details page - Ideas LIke button and total likes count display implementation with jquery ajax

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ def likes():
 
     return jsonify(result=count)
 
-#------------------------- App Launch -----------------------------------
+#------------------------- App Launch ------------------------------------
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key_230742'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
